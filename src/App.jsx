62bd690ded6444f0b45e852edaebe6726e9e6cb9
Madan <MadanH315140@gmail.com>
2026-07-02 21:35:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+   return (
+      <>
+         <body>
+            <div className=' min-h-screen flex justify-center items-center'>
+               <h1 class='text-3xl font-bold '>
+                  Hello <br /> Ahbishek Reddy!
+               </h1>
+            </div>
+         </body>
+      </>
+   );
+}
+
+export default App;
